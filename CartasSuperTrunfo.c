de17b697406b643a5c7292e68fd4753537bab1a6
yuridevii <dudatechoficial@gmail.com>
2025-05-20 15:00:11 -0300

    AventureiroCartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -62,7 +62,7 @@ int main(){
     scanf ("%d", &numeropontos2);
 
 
-    //Impressão das cartas
+    //Impressão das cartas 1
     printf("Carta 01:\n");
     printf("UF do estado: %s\n", estado1);
     printf("Código da carta: %s\n", codigo1);
@@ -71,7 +71,10 @@ int main(){
     printf("Área em km²: %.2f\n", area1);
     printf("PIB: %.2f\n", pib1);
     printf("Número de pontos turísticos: %d\n", numeropontos1);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", ppc1);
 
+    //Impressão das cartas 2
     printf("Carta 02:\n");
     printf("UF do estado: %s\n", estado2);
     printf("Código da carta: %s\n", codigo2);
@@ -80,5 +83,8 @@ int main(){
     printf("Área em km²: %.2f\n", area2);
     printf("PIB: %.2f\n", pib2);
     printf("Número de pontos turísticos: %d\n", numeropontos2);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais \n", ppc2);
+    
     return 0;
 }
